9ea129e4bec212947fcdbc75afd4bb971d000d4c
CyberImpXIII <jnelsenepstein@gmail.com>
2022-07-21 13:51:47 -0400

    removed misnamed files

--- fftBatch.py
+++ fftBatch.py
@@ -1,5 +1,10 @@
 import glob
-import graph_and_file_create.pt
+import graph_and_file_create
+import sys
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+
 files = glob.glob('./*.wav')
 for file in files:
     graph(file)
--- graph_and_file_create.py
+++ graph_and_file_create.py
@@ -1,12 +1,16 @@
+from asyncore import read, write
 import matplotlib.pyplot as graph
 from scipy.fftpack import fft
-from scipy.io import wavfile # get the api
+from scipy.io import wavfile  # get the api
+
+print('graphing')
+
 
 def graph(file):
-fs, data = wavfile.read(file)
-a = data.T[0:data.size]
-b=[(ele/2**8.)*2-1 for ele in a]
-c = fft(b)
-d = len(c)//2
-graph.plot(abs(c[:(d-1)]),'r') 
-graph.show()
+    fs, data = wavfile.read(file)
+    a = data.T[0:data.size]
+    b = [(ele/2**8.)*2-1 for ele in a]
+    c = fft(b)
+    d = len(c)//2
+    graph.plot(abs(c[:(d-1)]), 'r')
+    graph.show()
