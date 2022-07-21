b69a491e93ea3bdd045a4c8a7560639f90cd5a7f
CyberImpXIII <jnelsenepstein@gmail.com>
2022-07-21 14:16:55 -0400

    allowed for extra arguements

--- graph_and_file_create.py
+++ graph_and_file_create.py
@@ -1,16 +1,15 @@
 from asyncore import read, write
-import matplotlib.pyplot as graph
+import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.io import wavfile  # get the api
 
-print('graphing')
 
-
-def graph(file):
+def graph(file, output, input):
+    print('graphing', file)
     fs, data = wavfile.read(file)
     a = data.T[0:data.size]
     b = [(ele/2**8.)*2-1 for ele in a]
     c = fft(b)
     d = len(c)//2
-    graph.plot(abs(c[:(d-1)]), 'r')
-    graph.show()
+    plt.plot(abs(c[:(d-1)]), 'r')
+    plt.show()
