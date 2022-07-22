10d59ab47fff087154a78f475db429c5f67b16a2
CyberImpXIII <jnelsenepstein@gmail.com>
2022-07-21 20:32:54 -0400

    csv capability and some conditional logic

--- fftBatch.py
+++ fftBatch.py
@@ -1,24 +1,60 @@
 import glob
+
+from numpy import true_divide
 from graph_and_file_create import *
 import sys
 import os
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+input = './'
+output = './output'
+graphprint = True
+csvprint = True
 
-if glob.glob(sys.argv[1]):
-    print('placing graph files and fft csv files in', sys.argv[1])
-    output = sys.argv[1]
+for argument in sys.argv:
+    if("INPUT=" in argument.upper()):
+        if(argument.split('=')[1] == './' or argument.split('=')[1] == './input'):
+            print('no need to include' + argument.split('=')
+                  [1] + 'in your function call as it is supported by default')
+        input = argument.split('=')[1]
+    if("OUTPUT=" in argument.upper()):
+        if(argument.split('=')[1] == './output'):
+            print(
+                'no need to include ./output in your function call as it is supported by default')
+        output = argument.split('=')[1]
+    if('GRAPH=' in argument.upper()):
+        graphprint = argument.split('=')[1].lower() == 'true'
+    if('CSV=' in argument.upper()):
+        csvprint = argument.split('=')[1].lower() == 'true'
+
+if glob.glob(output):
+    if(graphprint == True & csvprint == True):
+        print('placing graph files and fft csv files in', output)
+    elif(graphprint == True):
+        print('placing graph files in', output)
+    elif(csvprint == True):
+        print('placing fft csv files in', output)
 else:
     print('output folder does not exist or is not valid, using default output folder')
-    print('placing graph files and fft csv files in output')
+    if(graphprint == True & csvprint == True):
+        print('placing graph files and fft csv files in', output)
+    elif(graphprint == True):
+        print('placing graph files in', output)
+    elif(csvprint == True):
+        print('placing csv files in', output)
+
     if(glob.glob('./output')[0] != './output'):
         print('creating output')
         os.mkdir('./output')
     output = './output'
-if glob.glob(sys.argv[2]):
-    print('placing graph files and fft csv files in', sys.argv[2])
-    input = sys.argv[2]
+
+if(graphprint and not glob.glob(output + '/graph')):
+    os.mkdir(output + '/graph')
+
+if(csvprint and not glob.glob(output + '/csv')):
+    os.mkdir(output + '/csv')
+
+if glob.glob(input):
+    print('placing graph files and fft csv files in', input)
 else:
     print('input folder does not exist or is not valid')
     if(glob.glob('./input')):
@@ -30,5 +66,6 @@ else:
 
 files = glob.glob(input + '*.wav')
 for file in files:
-    graph(file, file.replace('./', '').replace('.wav', ''), output, input)
+    graph(file, file.replace('./', '').replace('.wav', ''),
+          output, input, graphprint, csvprint)
 quit()
